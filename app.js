aaf796cd63522e5179f0c936f48effb5042fb099
Saidazizhon <asrarovs26@gmail.com>
2024-12-23 18:40:11 +0500

    tugadi

---
+++ app.js
@@ -0,0 +1,91 @@
+//1
+// function sumEvenNumbers(n: number): number {
+//     let sum = 0;
+//     for (let i = 0; i <= n; i += 2) {
+//         sum += i;
+//     }
+//     return sum;
+// }
+// console.log(sumEvenNumbers(7));
+//2
+// function booleanToNumber(value: boolean): number {
+//     return value ? 1 : 0;
+// }
+// console.log(booleanToNumber(true));
+// console.log(booleanToNumber(false)); 
+//3
+// function firstTwoLetters(name: string): string {
+//     return name.substring(0, 2);
+// }
+// console.log(firstTwoLetters("Laylo"));
+//4
+// function increaseAndRound(number: number): number {
+//     return Math.round(number + 2);
+// }
+// console.log(increaseAndRound(5.3));
+//5
+// function incrementAndStringify(num: number): string {
+//     return (num + 1).toString();
+// }
+// console.log(incrementAndStringify(4));
+//6
+// function printCharacters(word: string): void {
+//     for (const char of word) {
+//         console.log(char);
+//     }
+// }
+// printCharacters("salom"); 
+//7
+// function reverseCount(n: number): number[] {
+//     const result = [];
+//     for (let i = n; i >= -n; i--) {
+//         result.push(i);
+//     }
+//     return result;
+// }
+// console.log(reverseCount(2));
+//8
+// function numberToBoolean(num: number): boolean {
+//     return num === 1;
+// }
+// console.log(numberToBoolean(1));
+// console.log(numberToBoolean(0)); 
+//9
+// function introduce(name: string, nationality: string): string {
+//     return `My name is ${name}. I'm ${nationality};`}
+// console.log(introduce("Laylo", "uzbek")); 
+// let a:number = 5
+// let str:string = "Hello TypeScript"
+// let bool:boolean = true
+// let empty:undefined = undefined
+// let user:null = null
+// let numbers: number[] = [10,30]
+// interface CarType{
+//     title: string,
+//     price:number,
+//     year: number
+// }
+// const car: CarType = {
+//     title: "Tayota",
+//     price: 20_000,
+//     year: 2020
+// }
+// const car2: CarType = {
+//     title: "Mercedes",
+//     price: 30_000,
+//     year: 2022
+// }
+// const car3: CarType = {
+//     title: "BMW",
+//     price: 40_000,
+//     year: 2023
+// }
+// console.log(a);
+// console.log(str);
+// console.log(bool);
+// console.log(empty);
+// console.log(user);
+// console.log(numbers);
+// console.log(car);
+// console.log(car2);
+// console.log(car3);
