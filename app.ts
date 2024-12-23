d604ee1994fadd185c61c8c047fb9f677efd0007
Saidazizhon <asrarovs26@gmail.com>
2024-12-23 19:01:48 +0500

    tugadi

--- app.ts
+++ app.ts
@@ -1,4 +1,67 @@
+//1
+// function sumEvenNumbers(n: number): number {
+//     let sum = 0;
+//     for (let i = 0; i <= n; i += 2) {
+//         sum += i;
+//     }
+//     return sum;
+// }
+// console.log(sumEvenNumbers(7));
 
+//2
+// function booleanToNumber(value: boolean): number {
+//     return value ? 1 : 0;
+// }
+// console.log(booleanToNumber(true));
+// console.log(booleanToNumber(false)); 
+
+//3
+// function firstTwoLetters(name: string): string {
+//     return name.substring(0, 2);
+// }
+// console.log(firstTwoLetters("Laylo"));
+
+//4
+// function increaseAndRound(number: number): number {
+//     return Math.round(number + 2);
+// }
+// console.log(increaseAndRound(5.3));
+
+//5
+// function incrementAndStringify(num: number): string {
+//     return (num + 1).toString();
+// }
+// console.log(incrementAndStringify(4));
+
+//6
+// function printCharacters(word: string): void {
+//     for (const char of word) {
+//         console.log(char);
+//     }
+// }
+// printCharacters("salom"); 
+
+//7
+// function reverseCount(n: number): number[] {
+//     const result = [];
+//     for (let i = n; i >= -n; i--) {
+//         result.push(i);
+//     }
+//     return result;
+// }
+// console.log(reverseCount(2));
+
+//8
+// function numberToBoolean(num: number): boolean {
+//     return num === 1;
+// }
+// console.log(numberToBoolean(1));
+// console.log(numberToBoolean(0)); 
+
+//9
+// function introduce(name: string, nationality: string): string {
+//     return `My name is ${name}. I'm ${nationality};`}
+// console.log(introduce("Laylo", "uzbek")); 
 
 
 
